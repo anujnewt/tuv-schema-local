55ffc2d16dd679913b791de7ecf75ff11c61403f
NithishGundappaMS <nithishg@newtglobalcorp.com>
2026-05-21 13:24:22 +0530

    Changed for DBMS_SQL.RETURN_RESULT by adding an INOUT refcursor parameter

---
+++ usr/local/tomcat/dmap_extension_assets/Remediated_Code/ORCLDEV/FECXC/PROCEDURE/FECI_OBTENER_MONEDA_PR.sql
@@ -0,0 +1,15 @@
+create or replace procedure fecxc."feci_obtener_moneda_pr"  (INOUT feci_cursor refcursor) as $body$
+declare
+-- pgv moved types start
+-- pgv moved types end
+--feci_cursor refcursor;
+
+begin 
+
+open feci_cursor for
+select * from fecxc.feci_moneda_cat 
+where ind_estado =1;
+end;
+$body$
+language plpgsql
+;
