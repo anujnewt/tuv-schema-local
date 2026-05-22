fba8b836a98502fa96d785b09da2c5409c4a56e7
suryakt <suryakt@newtglobalcorp.com>
2026-05-22 17:50:47 +0530

    Changed for DBMS_SQL.RETURN_RESULT by adding an INOUT refcursor parameter

---
+++ usr/local/tomcat/dmap_extension_assets/Remediated_Code/ORCLDEV/FECXC/PROCEDURE/FECI_OBTENER_SEGM_PAIS_PR.sql
@@ -0,0 +1,14 @@
+create or replace procedure fecxc."feci_obtener_segm_pais_pr"  (INOUT feci_cursor refcursor) as $body$
+declare
+-- pgv moved types start
+-- pgv moved types end
+--feci_cursor refcursor;
+
+begin 
+
+open feci_cursor for
+select * from fecxc.feci_segm_pais_cat where ind_estado =1;
+end;
+$body$
+language plpgsql
+;
